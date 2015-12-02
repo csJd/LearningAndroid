d1145ead7456fdee411f39fc3ceec57e24f09e06
dd@Dell <ddwust@gmail.com>
2015-12-02 13:32:52 +0800

    add a tag;

--- Lib/app/src/main/java/dd/lib/MainActivity.java
+++ Lib/app/src/main/java/dd/lib/MainActivity.java
@@ -1,5 +1,7 @@
 package dd.lib;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -83,6 +85,9 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:10010"));
+            startActivity(intent);
 
         } else if (id == R.id.nav_slideshow) {
 
